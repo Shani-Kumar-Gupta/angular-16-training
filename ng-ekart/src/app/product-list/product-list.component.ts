51c4dc3d94a91f5695263705eb7cd8ac825c5156
Shani-Kumar-Gupta <shani.gupta_cse@outlook.com>
2024-10-20 00:19:52 +0530

    String Interpolation

--- ng-ekart/src/app/product-list/product-list.component.ts
+++ ng-ekart/src/app/product-list/product-list.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
+  productName: string = 'Iphone 15';
+  productPrice: number = 999;
+  productColor: string = 'Matte Black';
+  productDiscount: number = 8.5;
+  inStock: number = 10;
 
+  getDiscountedPrice(): number {
+    return this.productPrice - (this.productPrice * this.productDiscount) / 100;
+  }
 }
